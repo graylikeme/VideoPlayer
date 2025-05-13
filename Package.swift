93f89403c69d80d8a3789a39b46d0a781ddaacc9
Stanislav Ageev <stanislav@paperclip.co>
2025-05-13 17:21:52 +0500

    Update deps

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VideoPlayer",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "VideoPlayer",
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/wxxsw/GSPlayer.git", from: "0.2.27"),
+        .package(url: "https://github.com/wxxsw/GSPlayer.git", from: "0.2.28"),
     ],
     targets: [
         .target(
